3bc55079385e3ac25813c3aa31bc65ec2bacb006
KaimuSeino <skim07a19yki@icloud.com>
2024-08-02 21:10:47 +0900

    Userエンティティの追加

---
+++ app/src/Model/Entity/User.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * User Entity
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $email
+ * @property string $password
+ * @property string|null $role
+ * @property bool|null $delete_flg
+ * @property \Cake\I18n\FrozenTime|null $created
+ * @property \Cake\I18n\FrozenTime|null $modified
+ *
+ * @property \App\Model\Entity\Comment[] $comments
+ * @property \App\Model\Entity\Project[] $projects
+ * @property \App\Model\Entity\Task[] $tasks
+ */
+class User extends Entity
+{
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array<string, bool>
+     */
+    protected $_accessible = [
+        'username' => true,
+        'email' => true,
+        'password' => true,
+        'role' => true,
+        'delete_flg' => true,
+        'created' => true,
+        'modified' => true,
+        'comments' => true,
+        'projects' => true,
+        'tasks' => true,
+    ];
+
+    /**
+     * Fields that are excluded from JSON versions of the entity.
+     *
+     * @var array<string>
+     */
+    protected $_hidden = [
+        'password',
+    ];
+}
