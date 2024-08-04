b6036f185efd0f42a0650acd65b3dc773adc1640
KaimuSeino <skim07a19yki@icloud.com>
2024-08-04 18:20:26 +0900

    日本語化

--- app/src/Controller/PagesController.php
+++ app/src/Controller/PagesController.php
@@ -1,19 +1,6 @@
 <?php
 declare(strict_types=1);
 
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link      https://cakephp.org CakePHP(tm) Project
- * @since     0.2.9
- * @license   https://opensource.org/licenses/mit-license.php MIT License
- */
 namespace App\Controller;
 
 use Cake\Core\Configure;
@@ -24,9 +11,9 @@ use Cake\Http\Response;
 use Cake\View\Exception\MissingTemplateException;
 
 /**
- * Static content controller
+ * 静的コンテンツコントローラ
  *
- * This controller will render views from templates/Pages/
+ * このコントローラは `templates/pages/` からビューをレンダリングする。
  *
  * @link https://book.cakephp.org/4/en/controllers/pages-controller.html
  */
@@ -44,7 +31,7 @@ class PagesController extends AppController
     }
 
     /**
-     * Displays a view
+     * ビューの表示
      *
      * @param string ...$path Path segments.
      * @return \Cake\Http\Response|null
