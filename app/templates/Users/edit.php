7e61bbd8a9c2cae2aebc9eb42235215db3bc2ceb
KaimuSeino <skim07a19yki@icloud.com>
2024-08-05 09:10:24 +0900

    ユーザー削除機能を削除

--- app/templates/Users/edit.php
+++ app/templates/Users/edit.php
@@ -4,29 +4,24 @@
  * @var \App\Model\Entity\User $user
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $user->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
+<div>
+    <div>
         <div class="users form content">
             <?= $this->Form->create($user) ?>
             <fieldset>
-                <legend><?= __('Edit User') ?></legend>
-                <?php
-                    echo $this->Form->control('username');
-                    echo $this->Form->control('email');
-                ?>
+                <label class="label-title"><?= __('ユーザー編集') ?></label>
+                <div>
+                    <?php
+                        echo $this->Form->control('username', [
+                            'label' => 'ユーザー名'
+                        ]);
+                        echo $this->Form->control('email', [
+                            'label' => 'メールアドレス'
+                        ]);
+                    ?>
+                </div>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->button(__('更新'), ['class' => 'btn']) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
